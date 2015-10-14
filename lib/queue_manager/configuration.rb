32cbdfce7062452e8ead38b1e55bb0af56aa16a1
Mikhail Grachev <work@mgrachev.com>
2015-10-14 13:40:37 +0300

    Cleanup code

--- lib/queue_manager/configuration.rb
+++ lib/queue_manager/configuration.rb
@@ -2,21 +2,21 @@ module QueueManager
   class Configuration
 
     DEFAULT_CONFIG = {
-        wait:                     1,
-        delay:                    5,
-        timeout:                  15,
-        queue:                    'default_queue',
-        redis_connection_string:  'redis://localhost:6379/0',
-        worker:                   nil,
-        pid_file:                 File.join('/tmp', 'queue_manager.pid')
+      wait:                     1,
+      delay:                    5,
+      timeout:                  15,
+      queue:                    'default_queue',
+      redis_connection_string:  'redis://localhost:6379/0',
+      worker:                   nil,
+      pid_file:                 File.join('/tmp', 'queue_manager.pid')
     }
 
     DEFAULT_CONFIG.each_key do |key|
-      attr_writer key.to_sym
+      attr_writer key                                           # attr_writer :wait
 
-      define_method key do
-        instance_variable_get("@#{key}") || DEFAULT_CONFIG[key]
-      end
+      define_method key do                                      # def wait
+        instance_variable_get("@#{key}") || DEFAULT_CONFIG[key] #   @wait || DEFAULT_CONFIG[:wait]
+      end                                                       # end
     end
 
   end
